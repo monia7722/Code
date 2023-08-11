38983a2b4e7716088999387f8fe3d725229faa14
monia7722 <142011265+monia7722@users.noreply.github.com>
2023-08-11 12:56:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if(args.Lenght > 0)
+        {
+             System.Console.WriteLine("Hello " + args[0]);
+        }
+        else
+        {
+         System.Console.WriteLine("Hello");
+
+        }
+    }
+}
